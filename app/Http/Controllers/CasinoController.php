729b3f3db888b625ad204d716af290edaed6e48a
Balázs <venyigebalazs96@gmail.com>
2021-01-08 21:50:11 +0100

    update method added

--- app/Http/Controllers/CasinoController.php
+++ app/Http/Controllers/CasinoController.php
@@ -46,7 +46,37 @@ class CasinoController extends Controller
             $newKeyFeature->save();
         }
 
-        $casino->key_features = KeyFeatures::select('name')->where('casino_id', $casino->id)->get();
+        $casino->key_features = KeyFeatures::select('name', 'id')->where('casino_id', $casino->id)->get();
+        return $casino;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:casino,name,' . $id . '|string',
+            'rank' => 'required|unique:casino,rank,' . $id . '|integer',
+            'rating' => 'required|integer|max:5|min:1',
+            'key_features' => 'required|array',
+            'bonus' => 'required|string',
+            'link' => 'required|string',
+        ]);
+
+        $casino = Casino::find($id);
+        $casino->name = $request->name;
+        $casino->rank = $request->rank;
+        $casino->rating = $request->rating;
+        $casino->bonus = $request->bonus;
+        $casino->link = $request->link;
+        $casino->save();
+
+        $jsonKeyFeatures = $request->key_features;
+
+        foreach ($jsonKeyFeatures as $keyFeature) {
+            $databaseKeyFeature = KeyFeatures::find($keyFeature['id']);
+            $databaseKeyFeature->name = $keyFeature['name'];
+        }
+
+        $casino->key_features = $jsonKeyFeatures;
         return $casino;
     }
 }
